cf63cea6d263113c3947864a7d7d517a0642d55c
Lolikana <thomas.guiart@gmail.com>
2023-08-16 15:38:19 +0900

    install and setup prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+
+export default {
+	singleQuote: true,
+	arrowParens: 'avoid',
+	printWidth: 90,
+	trailingComma: 'none',
+	semi: true,
+	useTabs: true,
+	tabWidth: 2
+};
