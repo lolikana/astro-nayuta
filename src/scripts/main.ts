7698a56dfa9813c595729582f22a75cbb6e5270e
Lolikana <thomas.guiart@gmail.com>
2023-08-17 16:24:10 +0900

    header and navbar sticky

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,2 +1,14 @@
-const test = () => {};
-test();
+const header = document.querySelector('.header') as HTMLElement;
+const nav = document.querySelector('.header__nav') as HTMLElement;
+const navHeight = nav.getBoundingClientRect();
+const stickyNav = (entries: IntersectionObserverEntry[]) => {
+	const [entry] = entries;
+	if (!entry.isIntersecting) nav.classList.add('sticky');
+	if (entry.isIntersecting) nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+	root: null,
+	rootMargin: `-${navHeight.height}px`,
+	threshold: 0
+});
+if (header != null) headerObserver.observe(header);
