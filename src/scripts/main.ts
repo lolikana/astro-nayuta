d690198ab297e06192b867d248b273e4fd066f6c
Lolikana <thomas.guiart@gmail.com>
2023-08-18 13:59:54 +0900

    section top done

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,14 +1,18 @@
 const header = document.querySelector('.header') as HTMLElement;
 const nav = document.querySelector('.header__nav') as HTMLElement;
+const headerHeight = header.getBoundingClientRect();
 const navHeight = nav.getBoundingClientRect();
 const stickyNav = (entries: IntersectionObserverEntry[]) => {
 	const [entry] = entries;
+	console.log(entries);
+	console.log(entry.isIntersecting);
 	if (!entry.isIntersecting) nav.classList.add('sticky');
 	if (entry.isIntersecting) nav.classList.remove('sticky');
 };
 const headerObserver = new IntersectionObserver(stickyNav, {
 	root: null,
-	rootMargin: `-${navHeight.height}px`,
+	rootMargin: `-${headerHeight.height + 16 - navHeight.height}px`,
 	threshold: 0
 });
+console.log(headerObserver);
 if (header != null) headerObserver.observe(header);
