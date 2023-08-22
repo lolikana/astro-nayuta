7897b50cb61f1fe6acd7fba9f28f23f8956a515a
Lolikana <thomas.guiart@gmail.com>
2023-08-22 11:08:03 +0900

    add floor 2 map and fix some styles

--- src/components/homepage/datas.ts
+++ src/components/homepage/datas.ts
@@ -1,4 +1,4 @@
-import { TFloorMapData } from './types';
+import type { TFloorMapData } from './types';
 
 export const floorData: TFloorMapData[] = [
 	{
@@ -56,6 +56,49 @@ export const floorData: TFloorMapData[] = [
 				text: {
 					ja: 'The Room of Spirit and Time',
 					en: 'Rental Space another'
+				}
+			}
+		],
+		additional:
+			'※ THE VANISH, another のご利用は 1F フロントにて承ります。 空き状況はお電話にてお問い合わせください。'
+	},
+	{
+		floor: '2F',
+		details: [
+			{
+				number: 7,
+				icon: 'knife',
+				link: '#__blank',
+				text: {
+					ja: '瑞々しい野菜のフレンチランチ',
+					en: 'Vegan Restaurant Sui'
+				}
+			},
+			{
+				number: 8,
+				icon: 'knife',
+				link: '#__blank',
+				text: {
+					ja: 'フレッシュなスムージー',
+					en: 'Juice Bar Delight'
+				}
+			},
+			{
+				number: 9,
+				icon: 'knife',
+				link: '#__blank',
+				text: {
+					ja: 'こころを込めたお菓子とコーヒー',
+					en: 'cafe leter'
+				}
+			},
+			{
+				number: 10,
+				icon: 'book',
+				link: '#__blank',
+				text: {
+					ja: '深く耽溺するための図書スペース',
+					en: 'Book Station DIVE'
 				}
 			}
 		]
