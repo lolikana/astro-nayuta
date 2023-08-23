eb3ccff8011917e67cb27450d603b73e380ccae0
Lolikana <thomas.guiart@gmail.com>
2023-08-23 15:53:54 +0900

    start to implement html/css of data eatery

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -8,7 +8,9 @@ export type TEatery = {
 	floor: ['1F' | '2F'];
 	sns: ['Twitter' | 'Instagram'];
 	instagramUrl: string;
+	instagramName: string;
 	twitterUrl?: string;
+	twitterName: string;
 	dayOff?: ['日' | '月' | '火' | '水' | '木' | '金' | '土'];
 	businessInfo?: string;
 	dietary: ['Vegan' | 'Muslim Friendly' | 'Lacto-ovo Vegetarian'];
